28544504097505e667f4d2966025c0ba96b2d023
Lautaro <lautarorodriguezsk@gmail.com>
2023-10-31 07:34:35 -0300

    omito mostrar los Pokémon que no son default

--- src/hooks/usePokemonCard.jsx
+++ src/hooks/usePokemonCard.jsx
@@ -7,28 +7,35 @@ export const usePokemonCard = (selectedType) => {
   
     useEffect(() => {
         setIsLoading(true);
+
       const fetchPokemonData = async () => {
         try {
           const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=3000&offset=0');
+
           const { results } = await response.json();
           if (!response.ok) {
             throw new Error('Error al obtener lista de Pokémon');
           }
+
           const urls = results.map(result => result.url);
           const pokemonInfo = await Promise.all(urls.map(url => fetch(url).then(response => response.json())));
+
           const basicPokemonInfo = pokemonInfo.map(pokemon => ({
             id: pokemon.id,
             name: pokemon.name,
             sprite: pokemon.sprites.front_default,
+            default: pokemon.is_default,
             types: pokemon.types.map(type => type.type.name),
           }));
-  
-          setPokemonList(basicPokemonInfo);
+
+          const defaultPokemon = basicPokemonInfo.filter( ( pokemon ) => pokemon.default);
+
+          setPokemonList(defaultPokemon);
+
         } catch (error) {
           console.error('Error al obtener información de los Pokémon', error);
         } finally {
         setIsLoading(false);
-
         }
       };
   
