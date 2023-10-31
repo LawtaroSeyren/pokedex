1308b19c9f5f4c7a08673221024ebc3faa37e80c
Lautaro <lautarorodriguezsk@gmail.com>
2023-10-31 10:12:43 -0300

    detallando más estilos

--- src/components/PokeDex/PokeGrid/PokeCard.jsx
+++ src/components/PokeDex/PokeGrid/PokeCard.jsx
@@ -3,13 +3,15 @@ import { NavLink } from 'react-router-dom';
 
 const PokeCard = ({ name, id, sprite, types }) => {
   return (
-    <NavLink to={`/pokemon/${ id }`} >
-    <div key={id}>
+    <NavLink className="pokecard" to={`/pokemon/${ id }`} >
     <h2>{name}</h2>
     <p>ID: {id}</p>
     <img src={sprite} alt={name} />
-    {types.map((type) => <p key={type}>{type}</p>)}
-  </div>
+    <p>
+    {types.map((type) => <span className = { `typeBtn ${ type }` } >
+            { type }
+        </span>)}
+        </p>
   </NavLink>
   )
 }
